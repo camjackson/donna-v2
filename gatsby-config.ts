16b8a0a492ab96151c6e7b996dd22950619bda0f
Cam Jackson <1930451+camjackson@users.noreply.github.com>
2023-11-19 21:28:40 +1100

    Strip out all the crap

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,11 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Donna Comfort`,
+    siteUrl: `https://donnacomfort.com`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-postcss"],
 };
 
 export default config;
